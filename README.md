80cfefb44c5ed47404e4a30af132593d27117f55
acshameless <acshameless@gmail.com>
2024-11-23 02:30:51 +0800

    feat: init repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Algo 刷题
+
+> 记录工程师 shameless 的 Algo 刷题记录，不定期发布，欢迎关注，本 repo Fork 自工程师 Tw93 https://github.com/tw93/weekly，特此声明。
+
+
+* [第 00 题 - example](https://algo.shameless.top/posts/00-example)
---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const SITE = {
+  title: 'Algo Notes',
+  author: 'shameless',
+  description: '记录工程师 shameless 的 Algo 刷题记录，不定期发布，欢迎关注',
+  keywords: 'ac,shameless,Blog,Algo',
+  icon: 'https://aea62e6.webp.li/2024/11/favicon.png',
+  pic: 'https://aea62e6.webp.li/2024/11/favicon.png',
+  homePage: 'https://algo.shameless.top',
+  blogPage: 'https://ac.shameless.top/',
+  twitterId: 'acshameless',
+  githubId: 'acshameless',
+  repo: 'acshameless/algo-website',
+};
